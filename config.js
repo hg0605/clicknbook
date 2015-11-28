1838d5b0192a74dfac3bd104dd28c832b094710c
hg0605 <hg060595@gmail.com>
2015-11-28 12:22:40 +0530

    restored correct dbname

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports={
-	"database":"mongodb://hg0605:Harsh1859@ds049568.mongolab.com:49568/userstor",
+	"database":"mongodb://hg0605:Harsh1859@ds049568.mongolab.com:49568/userstory",
 	 "port":process.env.PORT || 3000,
 	"secretKey" : "YourSecretKey"
 }
