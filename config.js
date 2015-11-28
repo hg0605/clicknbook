47f11ea700208efb78e91f2d95589841e4f4a9b3
hg0605 <hg060595@gmail.com>
2015-11-28 12:21:09 +0530

    test

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports={
-	"database":"mongodb://hg0605:Harsh1859@ds049568.mongolab.com:49568/userstory",
+	"database":"mongodb://hg0605:Harsh1859@ds049568.mongolab.com:49568/userstor",
 	 "port":process.env.PORT || 3000,
 	"secretKey" : "YourSecretKey"
 }
